70af8d732d5028adc57dde5e473736d4fd900409
cuong6218 <cuong6218@domain.com>
2020-06-23 14:32:58 +0700

    update file gop mang

--- MergeMatrix/index.php
+++ MergeMatrix/index.php
@@ -1,14 +1,14 @@
 <?php
-    $arr1 = [1,2,3];
-    $arr2 = [4,5,6];
-    $arr3 = [];
+    $arr1 = [0,2,4];
+    $arr2 = [3,5,6,7];
+
     // $arr3 = array_merge($arr1, $arr2);
-    for ($i = 0; $i < 3; $i++){
-        $arr3[$i] = $arr1[$i];   
+    for ($i = 0; $i < count($arr1); $i++){
+        $arr3[$i] = $arr1[$i];  
     }
     
-    for ($j = 3; $j < 6; $j++){
-        $arr3[$j] = $arr2[$j-2];
+    for ($j = 0; $j < count($arr2); $j++){
+        $arr3[count($arr1)+$j] = $arr2[$j];
     }
     print_r ($arr3);
     
